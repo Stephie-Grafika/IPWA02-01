7b8251a42f70773f775be70f62f3ff6524978e55
Stephie-Grafika <164401918+Stephie-Grafika@users.noreply.github.com>
2024-10-21 23:58:04 +0200

    f:facet & h:dataTable

--- java/Co2emission.java
+++ java/Co2emission.java
@@ -1,23 +1,65 @@
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Named;
+/*import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;*/
 
-@Named
+@Named("co2emission")
 @RequestScoped
-public class Co2emission
+public class Co2emission implements Serializable
 {
-    private String land = "Deutschland";
+    private String land;
+    private String flagge;
+    private String emission2020;
+    private String emission2021;
+    private String emission2022;
+    private String emission2023;
 
-    public Co2emission()
-    {
+    public Co2emission(){
     }
 
-    public String getLand()
-    {
-        return land;
+    public String getLand(){
+    	return land;
     }
 
-    public void setLand(String land)
-    {
-        this.land = land;
+    public void setLand(String land){
+    	this.land = land;
+    }
+    
+    public String getFlagge(){
+    	return flagge;
+    }
+
+    public void setFlagge(String flagge){
+    	this.flagge = flagge;
+    }
+    
+    public String getEmission2020(){
+    	return emission2020;
+    }
+
+    public void setEmission2020(String emission2020){
+    	this.emission2020 = emission2020;
+    }
+    
+    public String getEmission2021(){
+    	return emission2021;
+    }
+
+    public void setEmission2021(String emission2021){
+    	this.emission2021 = emission2021;
+    }
+    
+    public String getEmission2022(){
+    	return emission2022;
+    }
+
+    public void setEmission2022(String emission2022){
+    	this.emission2022 = emission2022;
+    }
+    
+    public String getEmission2023(){
+    	return emission2023;
+    }
+
+    public void setEmission2023(String emission2023){
+    	this.emission2023 = emission2023;
     }
 }
